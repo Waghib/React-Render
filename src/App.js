dd6a1da890d7995ded857cf5a9bc9075759d1bb3
Waghib <Waghibahmad30@gmail.com>
2024-08-04 00:12:44 +0500

    react render - Parent and Child

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import UseReducer from './component/UseReducer/UseReducer';
 import { ObjectUseState } from './component/Immutable State/ObjectUseState';
+import { Parent } from './component/Parent Child/Parent';
 
 function App() {
   return (
     <div className="App">
-      <ObjectUseState />
+      <Parent />
     </div>
   );
 }
---
+++ src/component/Parent Child/Child.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Child = () => {
+    
+        console.log('render Child')
+  return (
+    <div>
+        Child Component
+    </div>
+  )
+}
