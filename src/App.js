d7ea7845cf48461fe65c1fe2b5ee7a38538480a6
Waghib <Waghibahmad30@gmail.com>
2024-08-05 23:03:09 +0500

    understood the react rendering process

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { ParentOne } from './component/Optimization/ParentOne';
 
 function App() {
   return (
     <div className="App">
-      <ParentOne/>
+      
     </div>
   );
 }
